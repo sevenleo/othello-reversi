3697bc55839f99dcfff27b6b526906e379522be1
Leonardo Neves <enfimleo@gmail.com>
2016-06-16 21:00:50 -0300

    criando tips 10%

--- Assets/OnClick.cs
+++ Assets/OnClick.cs
@@ -7,6 +7,7 @@ public class OnClick : MonoBehaviour {
     public GameObject Piece;
     public Material P1Color;
     public Material P2Color;
+    public Material TipColor;
 
 
 
@@ -24,36 +25,47 @@ public class OnClick : MonoBehaviour {
             CastRay();
         }
 
-
-        //TESTE - BUSCAR OBJETO NA TELA
-        if ( Input.GetKeyDown(KeyCode.L) ) {
-            GameObject.Find("A7").GetComponent<Renderer>().enabled = false;
-        }
     }
 
-    void CastRay()
-    {
+    void CastRay()    {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, 100))
         {
-            Debug.DrawLine(ray.origin, hit.point);
-            Debug.Log("Hit object: " + hit.collider.name);
-
-           /* if (hit.collider.tag == "piece")
-                hit.collider.GetComponent<Renderer>().enabled = !hit.collider.GetComponent<Renderer>().enabled;
-                */
+            //Debug.DrawLine(ray.origin, hit.point);
+            //Debug.Log("Hit object: " + hit.collider.name);
+            //GameObject.Find("NOME").GetComponent<Renderer>().enabled = false;
 
             if (hit.collider.tag == "board")  {
-                
+
+                //nomeia e colore a nova peca que sera criaca
+                Piece.name = "Piece_" + hit.collider.name;
                 if (p1turn) Piece.GetComponent<Renderer>().material = P1Color;
                 else Piece.GetComponent<Renderer>().material = P2Color;
 
+                //dica
+                Tips(hit.collider.name);
+
+                //coloca a peca na casa selecionada com uma distancia para visualizacao
                 Vector3 distance = new Vector3(0, 0, (float)-0.5);
                 Instantiate(Piece, hit.collider.transform.position+distance, hit.collider.transform.rotation);
+                
+                //desabilita a opcao de jogar naquela casa novamente
+                hit.collider.enabled = false;
 
+                //muda o turno
                 p1turn = !p1turn;
             }
         }
+    }
+
+    //D7 >> d8
+    void Tips(string piece) {
+        char row = piece[0]; //letra
+        char line = piece[1]; //numero
+        //line ++;
+        //GameObject.Find( row + line +"").GetComponent<Renderer>().material = TipColor;
+        GameObject.Find( row + "8").GetComponent<Renderer>().material = TipColor;
+        GameObject.Find(row + "1").GetComponent<Renderer>().material = TipColor;
     }
 }
